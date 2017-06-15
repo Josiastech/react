dbced3d220f97953f1574f436832237a92aa1dca
Josias Chocooj <jchocooj@katch.com>
2017-06-15 09:11:29 -0600

    Hello World! xD

---
+++ index.js
@@ -0,0 +1,18 @@
+/**
+ * The "render()" function will render JSX markup and
+ * place the resulting content into a DOM node. The "react"
+ * object isn't explicitly used here, but it's used
+ * by the transpiled JSX sorce.
+ */
+import React from 'react';
+import { render } from 'react-dom';
+
+/**
+ * Render the JSX markup. Notice the XML syntax
+ * mixed with JS? Thi is replaced y the 
+ * transpiler before it reached the browser.
+ */
+reder(
+    (<p>Hello, <strong>JSX</strong></p>),
+    document.getElementById('app')
+)
